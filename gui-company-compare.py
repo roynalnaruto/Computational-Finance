34ecb5f58434f0a5e7f7a087bccae97c3f726a22
Rohit Narurkar <roynalnaruto111@gmail.com>
2013-09-13 18:05:07 +0530

    GUI for Application
    Import the file 'comparison.py' in order to send/receive input/plot

--- gui-company-compare.py
+++ gui-company-compare.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt4 import QtGui, QtCore
+import comparison
 
 class Example(QtGui.QWidget):
     
@@ -19,18 +20,18 @@ class Example(QtGui.QWidget):
         self.label4 = QtGui.QLabel("End Date", self)
 
         #combo box for companies
-        comp1 = QtGui.QComboBox(self)
-        comp2 = QtGui.QComboBox(self)
+        comp1 = self.comp1 = QtGui.QComboBox(self)
+        comp2 = self.comp2 = QtGui.QComboBox(self)
 
         #combo box for start day, month, year
-        day1 = QtGui.QComboBox(self)
-        month1 = QtGui.QComboBox(self)
-        year1 = QtGui.QComboBox(self)
+        day1 = self.day1 = QtGui.QComboBox(self)
+        month1 = self.month1 = QtGui.QComboBox(self)
+        year1 = self.year1 = QtGui.QComboBox(self)
 
         #combo box for end day, month, year
-        day2 = QtGui.QComboBox(self)
-        month2 = QtGui.QComboBox(self)
-        year2 = QtGui.QComboBox(self)
+        day2 = self.day2 = QtGui.QComboBox(self)
+        month2 = self.month2 = QtGui.QComboBox(self)
+        year2 = self.year2 = QtGui.QComboBox(self)
 
         #get list of companies & add to combo
         ls_comp = []
@@ -75,22 +76,22 @@ class Example(QtGui.QWidget):
         comp2.move(200, 200)
         comp2.resize(100, 50)
 
-        day1.move(100, 350)
-        day1.resize(40, 30)
+        day1.move(100, 330)
+        day1.resize(50, 30)
 
-        day2.move(100, 450)
-        day2.resize(40, 30)
+        day2.move(100, 430)
+        day2.resize(50, 30)
 
-        month1.move(160, 350)
-        month1.resize(40, 30)
+        month1.move(160, 330)
+        month1.resize(50, 30)
 
-        month2.move(160, 450)
-        month2.resize(40, 30)
+        month2.move(160, 430)
+        month2.resize(50, 30)
 
-        year1.move(220, 350)
+        year1.move(220, 330)
         year1.resize(80, 30)
 
-        year2.move(220, 450)
+        year2.move(220, 430)
         year2.resize(80, 30)
         #done move & resize combo boxes
 
@@ -100,9 +101,9 @@ class Example(QtGui.QWidget):
         self.label2.move(100, 200)
         self.label2.resize(100, 50)
 
-        self.label3.move(150, 300)
+        self.label3.move(150, 280)
         self.label3.resize(100, 50)
-        self.label4.move(150, 400)
+        self.label4.move(150, 380)
         self.label4.resize(100, 50)
         #done move & resize labels
 
@@ -110,12 +111,67 @@ class Example(QtGui.QWidget):
         self.resize(900, 600)
         self.center()
 
+        #make 'compare' button to execute backend code
+        self.btn = QtGui.QPushButton('Compare', self)
+        self.btn.setToolTip('<b>compare stocks</b>')
+        self.btn.resize(self.btn.sizeHint())
+        self.btn.move(150, 500)
+
+        #make a call to the function calculate
+        self.btn.clicked.connect(self.calculate)
+        
+        #make labels to display the images as pixmaps
+        self.normalized_label = QtGui.QLabel(self)
+        self.scatter_label = QtGui.QLabel(self)
+
+        #move and resize labels (images)
+        self.normalized_label.move(440, 50)
+        self.normalized_label.resize(320, 240)
+        self.scatter_label.move(440, 310)
+        self.scatter_label.resize(320, 240)
+
+        #set the scaled content of image in case resolutions don't match
+        self.normalized_label.setScaledContents(True)
+        self.scatter_label.setScaledContents(True)
+
         #give title
-        self.setWindowTitle('TheElancer')
-        self.show()
+        self.setWindowTitle('The Elancer')
+
+
+    def calculate(self):
+        
+        #get input data from various combo boxes
+        start_day = int(self.day1.currentText())
+        start_month = int(self.month1.currentText())
+        start_year = int(self.year1.currentText())
+        end_day = int(self.day2.currentText())
+        end_month = int(self.month2.currentText())
+        end_year = int(self.year2.currentText())
+
+        #make list of companies to be compared
+        list_companies = []
+        company_1 = str(self.comp1.currentText())
+        company_2 = str(self.comp2.currentText())
+        list_companies.append(company_1)
+        list_companies.append(company_2)
+
+        #call the 'compare' function from the imported file 'comparison.py'
+        comparison.compare(start_day, start_month, start_year, end_day, end_month, end_year, list_companies)
+
+        #get pixmaps from saved files
+        normalized_returns = QtGui.QPixmap('normalized_returns.jpg')
+        scatter1v2 = QtGui.QPixmap('scatter1v2.jpg')
+
+        #set the frames for labels & display pixmaps
+        self.normalized_label.setFrameStyle(QtGui.QFrame.Panel)
+        self.normalized_label.setPixmap(normalized_returns)
+        self.scatter_label.setFrameStyle(QtGui.QFrame.Panel)
+        self.scatter_label.setPixmap(scatter1v2)
+
 
     def center(self):
         
+        #for centering of window
         qr = self.frameGeometry()
         cp = QtGui.QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
@@ -126,6 +182,7 @@ def main():
     
     app = QtGui.QApplication(sys.argv)
     ex = Example()
+    ex.show()
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
